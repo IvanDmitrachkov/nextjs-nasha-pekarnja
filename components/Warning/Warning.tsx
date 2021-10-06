1e5d5d277d9275e866a7eeaf3e20a380a0368ac4
DenisDmitrachkov <Dmitra4kovdenis@gmail.com>
2021-10-06 16:11:50 +0300

    test

--- components/Warning/Warning.tsx
+++ components/Warning/Warning.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 const Warning: React.FC = () => {
   return (
     <div className='text-16 text-white p-24 rounded inline-block bg-red'>
-      dsfnsdf
+      dsfnsdfgf
     </div>
   )
 }
